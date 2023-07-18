44845a98d2306ac3f5b69499de3686d81a4046d2
ahadsenja <hamdanilm96@gmail.com>
2023-07-18 23:28:07 +0800

    create posts dummy data

---
+++ src/app/helper/datas.ts
@@ -0,0 +1,50 @@
+export const posts = [
+  {
+    uuid: "1post-01",
+    title: "Macet berkepanjangan di jalur bypass lombok, lombok tengah menuju mataram",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in, animi culpa doloribus delectus repellendus accusamus doloremque minus id? Ullam ea dolore reprehenderit enim libero quisquam, incidunt adipisci quaerat pariatur?",
+    featuredImage: "https://picsum.photos/300/200",
+    category: "News",
+    author: "Admin",
+  },
+  {
+    uuid: "2post-02",
+    title: "Macet berkepanjangan di jalur bypass lombok, lombok tengah menuju mataram",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in, animi culpa doloribus delectus repellendus accusamus doloremque minus id? Ullam ea dolore reprehenderit enim libero quisquam, incidunt adipisci quaerat pariatur?",
+    featuredImage: "https://picsum.photos/300/200",
+    category: "News",
+    author: "Admin",
+  },
+  {
+    uuid: "3post-03",
+    title: "Macet berkepanjangan di jalur bypass lombok, lombok tengah menuju mataram",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in, animi culpa doloribus delectus repellendus accusamus doloremque minus id? Ullam ea dolore reprehenderit enim libero quisquam, incidunt adipisci quaerat pariatur?",
+    featuredImage: "https://picsum.photos/300/200",
+    category: "News",
+    author: "Admin",
+  },
+  {
+    uuid: "4post-04",
+    title: "Macet berkepanjangan di jalur bypass lombok, lombok tengah menuju mataram",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in, animi culpa doloribus delectus repellendus accusamus doloremque minus id? Ullam ea dolore reprehenderit enim libero quisquam, incidunt adipisci quaerat pariatur?",
+    featuredImage: "https://picsum.photos/300/200",
+    category: "News",
+    author: "Admin",
+  },
+  {
+    uuid: "5post-05",
+    title: "Macet berkepanjangan di jalur bypass lombok, lombok tengah menuju mataram",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in, animi culpa doloribus delectus repellendus accusamus doloremque minus id? Ullam ea dolore reprehenderit enim libero quisquam, incidunt adipisci quaerat pariatur?",
+    featuredImage: "https://picsum.photos/300/200",
+    category: "News",
+    author: "Admin",
+  },
+  {
+    uuid: "6post-06",
+    title: "Macet berkepanjangan di jalur bypass lombok, lombok tengah menuju mataram",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in, animi culpa doloribus delectus repellendus accusamus doloremque minus id? Ullam ea dolore reprehenderit enim libero quisquam, incidunt adipisci quaerat pariatur?",
+    featuredImage: "https://picsum.photos/300/200",
+    category: "News",
+    author: "Admin",
+  }
+]
