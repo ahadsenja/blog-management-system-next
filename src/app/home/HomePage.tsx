d73f95e93fc59b9876134fbbe5d5fd74e490c27b
ahadsenja <hamdanilm96@gmail.com>
2023-07-19 22:19:24 +0800

    chore: update home page to call navbar component

--- src/app/home/HomePage.tsx
+++ src/app/home/HomePage.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import Link from 'next/link'
+import Image from 'next/image'
+import Navbar from '../components/FrontPage/Navbar'
 
 export default function HomePage({ posts }: { posts: any[] }) {
   return (
     <>
-      <nav className='container mx-auto flex justify-between py-8'>
-        <Link href={"/blog"}>
-          <h2 className='text-2xl font-bold'>Blogi</h2>
-        </Link>
-        <Link href={"/login"} className='px-8 py-2 bg-blue-500 rounded'>Login</Link>
-      </nav>
+      <Navbar />
 
       <div className='hero container mx-auto h-96 mb-10 rounded'>
         <h1 className='text-3xl font-medium pt-20'>Blogi is a Blog management system</h1>
@@ -19,7 +15,7 @@ export default function HomePage({ posts }: { posts: any[] }) {
         {
           posts.map((post, index) => {
             return <div key={index} className='w-1/3 pr-8 pb-12'>
-              <img className='w-full mb-4' src={post.featuredImage} alt={post.title} />
+              <Image className='w-full mb-4' src={post.featuredImage} alt={post.title} width={300} height={200} />
               <h2 className='w-full text-2xl font-bold mb-4'>{post.title}</h2>
               <p className='w-full'>{post.content}</p>
             </div>
