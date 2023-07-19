c952975a50261d3173047c321a31c5186221249e
ahadsenja <hamdanilm96@gmail.com>
2023-07-19 22:19:01 +0800

    chore: move element of component navbar

---
+++ src/app/components/FrontPage/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className='container mx-auto flex justify-between py-8'>
+        <Link href={"/blog"}>
+          <h2 className='text-2xl font-bold'>Blogi</h2>
+        </Link>
+        <Link href={"/login"} className='px-8 py-2 bg-blue-500 rounded'>Login</Link>
+      </nav>
+    </>
+  )
+}
