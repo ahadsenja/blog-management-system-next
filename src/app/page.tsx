197385d89e458bf2d55189fd7719a8bf16721a4a
ahadsenja <hamdanilm96@gmail.com>
2023-07-24 00:39:47 +0800

    chore: update component name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+'use client'
 import { posts } from './helper/datas'
 import HomePage from './home/HomePage'
 
-export default function Home() {
+export default function Page() {
   return (
     <>
       <HomePage posts={posts} />
