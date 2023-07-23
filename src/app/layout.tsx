e47f05575fd4227d64e2bb90f69d3bc11733aac7
ahadsenja <hamdanilm96@gmail.com>
2023-07-24 00:42:01 +0800

    chore: update default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from './components/Dashboard/Navbar'
+import Sidebar from './components/Dashboard/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='flex'>
+          <Sidebar />
+          <div className='flex-1'>
+            <Navbar />
+            <div className='p-8'>
+              {children}
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
