a617cd52202e9b403dc9a137ed3f387182434a25
ahadsenja <hamdanilm96@gmail.com>
2023-07-24 00:34:55 +0800

    chore: add sidebar menu and link

---
+++ src/app/components/Dashboard/Sidebar.tsx
@@ -0,0 +1,29 @@
+'use client'
+import Link from 'next/link'
+
+export default function Sidebar() {
+  return (
+    <>
+      <aside className='h-screen w-60 px-8 bg-gray-700 py-8 static'>
+        <div className='text-3xl font-bold mb-12'>Blogi</div>
+        <nav className='flex flex-col gap-4 text-white'>
+          <ul>
+            <li>
+              <Link href={'/dashboard'}>Dashboard</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link href={'/posts'}>Posts</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link href={'/users'}>Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
+  )
+}
