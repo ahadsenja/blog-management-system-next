cb3ec30220d124b9ab230d9a732703f1fd8992a2
ahadsenja <hamdanilm96@gmail.com>
2023-07-24 23:29:25 +0800

    style: update navbar and sidebar styling position

--- src/app/components/Dashboard/Navbar.tsx
+++ src/app/components/Dashboard/Navbar.tsx
@@ -10,8 +10,8 @@ export default function Navbar() {
   }
   return (
     <>
-      <nav className='flex justify-end static w-full py-6 text-white bg-gray-600 pr-8'>
-        <div className='text-md'>
+      <nav className='w-full py-6 text-white bg-gray-600 pr-8 fixed top-0 left-0 right-0'>
+        <div className='flex justify-end text-md'>
           <button onClick={handleLogout}>Logout</button>
         </div>
       </nav>
