24caa4be04d94b5e38fb40d1c21251fdad277767
ahadsenja <hamdanilm96@gmail.com>
2023-07-31 05:06:12 +0800

    update interface file

---
+++ src/app/types/BlogInterface.ts
@@ -0,0 +1,13 @@
+export interface PostIface {
+  id: string,
+  title: string,
+  content: string,
+  featuredImage: string,
+  category: string,
+  author: string
+}
+
+export interface UserIface {
+  username: string,
+  password: string
+}
