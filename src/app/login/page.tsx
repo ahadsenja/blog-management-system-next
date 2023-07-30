1abadbbcbc01b2bcf422be0030569255bb6a8f25
ahadsenja <hamdanilm96@gmail.com>
2023-07-31 05:03:36 +0800

    chore: delete unnecessary line of codes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
+
 import { useState } from 'react';
 import Login from './Login'
-import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
   const [username, setUsername] = useState('');
