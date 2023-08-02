ff03154141049c3e5d347ca571cb1eb420924a4e
ahadsenja <hamdanilm96@gmail.com>
2023-08-02 15:16:40 +0800

    chore: update next config and yarn file

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      }
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/protected',
-        destination: '/protected'
-      }
-    ]
+  env: {
+    API_URL: process.env.API_URL,
   }
 }
 
